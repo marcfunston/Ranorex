04ee29c27f04923f6fbebeb10d2c516224c61df0
M <marc.funston@gmail.com>
2021-05-21 14:53:44 -0500

    Update index.php
    Cleaning up html output.

--- index.php
+++ index.php
@@ -102,22 +102,20 @@ $nextPage = $page + 1;
 
 
 ?>
-<div class="w3-row">
-<div class="w3-half w3-container">
-    <form action="index.php" method="post">
-        <input type="hidden" name="page" value=<?php echo"$previousPage"; ?>>
-        <button class="w3-button w3-black w3-padding-large w3-margin-bottom w3-right" input type="submit"
-        name="submit" value="Next" />Previous</button>
-    </form>
-</div>
-<div class="w3-half w3-container">
-    <form action="index.php" method="post">
-        <input type="hidden" name="page" value=<?php echo"$nextPage"; ?>>
-        <button class="w3-button w3-black w3-padding-large w3-margin-bottom" input type="submit"
-        name="submit" value="Next" />Next</button>
-    </form>
-</div>
-</div>
+        <div class="w3-row">
+            <div class="w3-half w3-container">
+              <form action="index.php" method="post">
+                <input type="hidden" name="page" value=12>
+                <button class="w3-button w3-black w3-padding-large w3-margin-bottom w3-right" input type="submit" name="submit" value="Next" />Previous</button>
+              </form>
+            </div>
+            <div class="w3-half w3-container">
+              <form action="index.php" method="post">
+                <input type="hidden" name="page" value=14>
+                <button class="w3-button w3-black w3-padding-large w3-margin-bottom" input type="submit" name="submit" value="Next" />Next</button>
+              </form>
+            </div>
+          </div>
 <?php
 
 // footer to include pages
