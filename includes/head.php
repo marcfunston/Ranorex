d728220d16273420f9e09d552aab0544dfacd0fd
M <marc.funston@gmail.com>
2021-05-21 14:53:04 -0500

    Update head.php
    Cleaning up html output

--- includes/head.php
+++ includes/head.php
@@ -11,30 +11,28 @@
 ?>
 <!DOCTYPE html>
 <html>
-<title>Ranorex</title>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="style/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
-<style>
-body,
-h1,
-h2,
-h3,
-h4,
-h5 {
-    font-family: "Raleway", sans-serif
-}
-</style>
-<head>
-  <script>
-    function myFunction() {var x = document.getElementById("DEMO"); var y = document.getElementById("DEMO2");
+  <title>Ranorex</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style/w3.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+  <style>
+    body,
+    h1,
+    h2,
+    h3,
+    h4,
+    h5 {font-family: "Raleway", sans-serif}
+  </style>
+  <head>
+    <script>
+      function myFunction() {var x = document.getElementById("DEMO"); var y = document.getElementById("DEMO2");
         if (x.className.indexOf("w3-show") == -1) {x.className += " w3-show"; y.className += " w3-hide";
         } else {x.className = x.className.replace(" w3-show", ""); y.className = y.className.replace(" w3-hide", "");}}
-    function w3_open() {document.getElementById("mySidebar").style.display = "block";}
-    function w3_close() {document.getElementById("mySidebar").style.display = "none";}
-  </script>
-</head>
+      function w3_open() {document.getElementById("mySidebar").style.display = "block";}
+      function w3_close() {document.getElementById("mySidebar").style.display = "none";}
+    </script>
+  </head>
 <?php  
 if (!isset($_COOKIE['page'])) {
     setcookie ('page', 1);	
@@ -42,16 +40,16 @@ if (!isset($_COOKIE['page'])) {
     $page = $_COOKIE['page'];
   }
 ?>
-<body class="w3-light-grey">
-  <div class="w3-content" style="max-width:1000px">
-    <header class="w3-container w3-center w3-padding-32">
+    <body class="w3-light-grey">
+      <div class="w3-content" style="max-width:1000px">
+      <header class="w3-container w3-center w3-padding-32">
         <div class="w3-row">
-            <div>
-                <h1><b>Ranorex Flashcards</b></h1>
-                <h2><b>Knowledge</b></h2>
-                <h3 class="w3-tag">The Gateway to a Bigger World!</h3>
-            </div>
+          <div>
+            <h1><b>Ranorex Flashcards</b></h1>
+            <h2><b>Knowledge</b></h2>
+            <h3 class="w3-tag">The Gateway to a Bigger World!</h3>
+          </div>
         </div>
-    </header>
-    <div class="w3-row">
-      <div class="w3-col l8 s12">
+        </header>
+        <div class="w3-row">
+        <div class="w3-col l8 s12">
