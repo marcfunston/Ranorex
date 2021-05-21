4301ca1bbea67272ffc9e9ddef2bedfc62990054
M <marc.funston@gmail.com>
2021-05-21 14:52:14 -0500

    Update footer.php
    Cleaning html output.

--- includes/footer.php
+++ includes/footer.php
@@ -10,17 +10,17 @@
  */
 
 ?>
-  </div>
-   <div class="w3-col l4">
-    <div class="w3-card w3-margin w3-center w3-padding-16">
-        <img src="images/ISS_logo.png" style="width:80%">
-        <h3><b>Inter State Studio</b></h3>
-        <h3><b>Learning</b></h3>
-    </div>
-   </div>
-  </div><br>
- </div>
-  <footer class="w3-container w3-light-grey w3-padding-16 w3-margin-top">
-  </footer>
- </body>
+        </div>
+          <div class="w3-col l4">
+            <div class="w3-card w3-margin w3-center w3-padding-16">
+              <img src="images/ISS_logo.png" style="width:80%">
+              <h3><b>Inter State Studio</b></h3>
+              <h3><b>Learning</b></h3>
+            </div>
+          </div>
+        </div><br>
+      </div>
+      <footer class="w3-container w3-light-grey w3-padding-16 w3-margin-top">
+      </footer>
+    </body>
 </html>
